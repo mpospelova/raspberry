161f775bb79c3df2c5cd9e7de44bd95347c3fd11
mpospelova <mariyapospelova@gmail.com>
2021-10-31 09:38:27 +0000

    Adding functionallity to capture pictures and video

--- video.py
+++ video.py
@@ -1,81 +1,22 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
+import picamera
 from time import sleep
-from picamera import PiCamera
-import os
-import sys
-import datetime
-import RPi.GPIO as GPIO
-import os
+from subprocess import call
 
-class driveRecoder():
-    try:
-        #Button setup
-        SWITCH_PIN=21
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(SWITCH_PIN,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+# Setup the camera
+with picamera.PiCamera() as camera:
+    # Start recording
+    camera.start_recording("pictures/pythonVideo.h264")
+    sleep(5)
+    # Stop recording
+    camera.stop_recording()
 
-        #Camera setup
-        camera = PiCamera()
-        resolution_width = 1920
-        resolution_height = 1080
-        record_time=60
-        camera.resolution = (resolution_width, resolution_height)
-        camera.framerate = 30
-        camera.led = False
+# The camera is now closed.
 
-        def shutdown():
-            print ("shutdown)"
-            # os.system("sudo shutdown -h now")
-
-        #Callback function
-        #Callback function
-	def stopRecording():
-	    camera.stop_recording()
-	    print("録画終了")
-	    #I want to power down after stop recording 
-	    #GPIO.cleanup()
-	    #shutdown()
-
-
-        GPIO.add_event_detect(SWITCH_PIN,GPIO.FALLING)
-        GPIO.add_event_callback(SWITCH_PIN,stopRecording)
-
-        while True:
-            try:
-                print("録画開始")
-
-                #setup file name and directory path to save
-                now = datetime.datetime.now()
-                dir_name = now.strftime('%Y%m%d')
-                dir_path = '/home/admini/Video/'+dir_name
-                file_name = now.strftime('%H%M%S')
-
-                #Make each date folder if not exist folder
-                if not os.path.exists(dir_path):
-                        os.makedirs(dir_path)
-                        os.chmod(dir_path, 0777)
-
-                #Recording 10 min the loop
-                camera.start_recording(dir_path+'/'+file_name+'.h264')
-                camera.wait_recording(record_time)
-                camera.stop_recording()
-                print("録画終了")
-                    sleep(2)
-            except KeyboardInterrupt:
-                camera.stop_recording()
-                break
-            finally:
-                pass
-        print("録画終了")
-    except KeyboardInterrupt:
-        print("ctl+c終了")
-        GPIO.cleanup()
-        sys.exit(0)
-    finally:
-        pass
-    print("終了")
-    GPIO.cleanup()
-    sys.exit(0)
+print("We are going to convert the video.")
+# Define the command we want to execute.
+command = "MP4Box -add pythonVideo.h264 convertedVideo.mp4"
+# Execute our command
+call([command], shell=True)
+# Video converted.
+print("Video converted.")
 
